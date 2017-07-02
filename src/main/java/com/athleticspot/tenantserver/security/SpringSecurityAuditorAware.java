99e4a987e9fbb837ebae6ae80dcd8dc747be6423
tomkasp@gmail.com <tomkasp@gmail.com>
2017-07-02 15:15:57 +0200

    init commit

---
+++ src/main/java/com/athleticspot/tenantserver/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,19 @@
+package com.athleticspot.tenantserver.security;
+
+import com.athleticspot.tenantserver.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = SecurityUtils.getCurrentUserLogin();
+        return userName != null ? userName : Constants.SYSTEM_ACCOUNT;
+    }
+}
